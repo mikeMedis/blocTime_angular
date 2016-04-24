47e3901feecf8e2f1fa7f8dc08829e5e78efd468
Michael Medis <TheMedisGroup@gmail.com>
2016-04-24 16:04:32 -0400

    bootstrapAdded

--- dist/scripts/factories/Tasks.js
+++ dist/scripts/factories/Tasks.js
@@ -8,12 +8,7 @@ function Tasks($firebaseArray) {
 	var firebaseRef = new Firebase ("https://bloctimemikemedis.firebaseio.com/tasks");
 	var tasks = $firebaseArray(firebaseRef);
 
-	function deleteTasks() {
-			firebaseRef.remove();
-	};
-
 	return {
 		all: tasks,
-		deleteTasks: deleteTasks
 	}
 };
